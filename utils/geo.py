226f26eee8df4051fe4598e0508bb60d2f06a2b3
seohuiwon11 <shwsrh00@gmail.com>
2025-10-30 22:33:23 +0900

    feat: 좌표 문자열에서 공백이나 특수문자 제거
    좌표 문자열에서 공백이나 특수문자 제거 하는 함수 구현

--- utils/geo.py
+++ utils/geo.py
@@ -1,9 +1,14 @@
 import re
 
+# 위도/경도가 유효 범위 내에 있는지 검사
 def is_valid_coordinate(lat, lon):
-    # 위도/경도가 유효 범위 내에 있는지 검사
     try:
         lat, lon = float(lat), float(lon)
         return -90 <= lat <= 90 and -180 <= lon <= 180
     except ValueError:
         return False
+    
+    
+# 좌표 문자열에서 공백이나 특수문자 제거
+def clean_coordinate_format(coord):
+    return re.sub(r"[^\d\.\-]", "", coord)
